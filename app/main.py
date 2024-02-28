88dc117fe3f85738f71fa3c1226fb9884fac372e
Loïc Poisot <lpoisot@customsbridge.fr>
2024-02-28 13:03:52 +0100

    path parameters + custom responses

--- app/main.py
+++ app/main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 
 app = FastAPI()
+
+users = [
+    {   
+        "id": 1,
+        "name": "John",
+        "age": 28
+    },
+    {
+        "id": 2,
+        "name": "Jane",
+        "age": 32
+    },
+    {
+        "id": 3,
+        "name": "Doe",
+        "age": 45
+    },
+    {
+        "id": 4,
+        "name": "Smith",
+        "age": 22
+    }
+]
 
 class RootReturnObj(BaseModel):
     Hello: str
@@ -14,3 +37,25 @@ async def helloWorld() -> RootReturnObj:
 @app.get("/say-hello-john")
 async def helloJohn():
     return "Hello John!"
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+
+@app.get("/users")
+async def getUsers() -> list[User]:
+    """
+    Endpoint to return all users
+    """
+    return users
+
+@app.get("/users/{user_id}", responses={404: {"model": str}})
+async def getUser(user_id: int) -> User:
+    for user in users:
+        if user["id"] == user_id:
+            return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
