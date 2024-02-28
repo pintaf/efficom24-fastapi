c3999550317b886937768e0d64fa3fec37168efb
Loïc Poisot <lpoisot@customsbridge.fr>
2024-02-28 12:44:51 +0100

    added concept of pydantic models

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class RootReturnObj(BaseModel):
+    Hello: str
 
 @app.get("/")
-def read_root():
+async def helloWorld() -> RootReturnObj:
     return {"Hello": "World"}
+
+
+@app.get("/say-hello-john")
+async def helloJohn():
+    return "Hello John!"
